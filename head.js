be538748a21e4d9d44da7289c45ea3f35ee80427
Marina Bekker <mshooch@gmail.com>
2022-01-26 21:35:13 -0500

    added head function

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+
+  //Function takes in two variables and compares them if they're exactly equal.
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+//function to return the first item in a given array.
+const head = function(arr) {
+  return arr[0];
+}
+
+//TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), 5);
