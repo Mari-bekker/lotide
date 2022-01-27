632826a2ede598a6da4a3eb26e15dfb2105abe78
Marina Bekker <mshooch@gmail.com>
2022-01-26 21:17:22 -0500

    added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+
+  //Function takes in two variables and compares them if they're exactly equal.
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("test", "test");
